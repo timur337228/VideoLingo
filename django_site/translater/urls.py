6b93ea8917a96a03693aadf8ab4843899735e425
Timur <timur337228@yandex.ru>
2026-05-20 12:25:53 +0900

    edit url video

--- django_site/translater/urls.py
+++ django_site/translater/urls.py
@@ -4,5 +4,5 @@ from translater.views import get_my_videos, translate_status, upload_video
 urlpatterns = [
     path("upload-video/", upload_video, name="upload_video"),
     path("my-videos/", get_my_videos, name="my_videos"),
-    path("translate-status/<int:video_id>/", translate_status, name="translate_status"),
+    path("video/<int:video_id>/", translate_status, name="translate_status"),
 ]
